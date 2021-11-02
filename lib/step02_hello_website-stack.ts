cd9ebab3871773eb5c48709dfc2218cd3340fca0
NaumanMunir9 <naumanmunir9@gmail.com>
2021-11-02 20:05:44 +0500

    s3, s3deployment. cloudfront, cloudfront-origins

--- lib/step02_hello_website-stack.ts
+++ lib/step02_hello_website-stack.ts
@@ -1,9 +1,32 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 
 export class Step02HelloWebsiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+    });
+
+    const distribution = new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: { origin: new origins.S3Origin(websiteBucket) },
+    });
+
+    const display = new cdk.CfnOutput(this, "output", {
+      value: distribution.domainName,
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("./website-dist")],
+      destinationBucket: websiteBucket,
+      destinationKeyPrefix: "web/static", // optional prefix in destination bucket
+      distribution,
+    });
   }
 }
